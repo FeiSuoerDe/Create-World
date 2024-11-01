bc75462ca0dbd04a3083f43487b1697be3979920
fi <2153602387@qq.com>
2024-11-01 11:44:03 +0800

    优化WorldTileMap,整理代码,和添加注释

--- Sprit/WorldSprite/WorldTileMap.cs
+++ Sprit/WorldSprite/WorldTileMap.cs
@@ -4,43 +4,29 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-
 public class WorldTileMap : MonoBehaviour
 {
-    // 地图的Tilemap组件
     [Header("Tilemap")]
     public Tilemap tilemap;
 
-    // 地图的最大尺寸
+    [Header("Map Size")]
     public Vector2Int maxMapSize;
-    [Header("seed")]
-    // 随机种子
-    public int seed;
 
-    // 是否使用自定义种子
-    public Boolean RandomSeed;
+    [Header("Seed")]
+    public int seed;
+    public bool useCustomSeed;
 
     [Header("Tiles")]
-    // 深海瓷砖
     public TileBase DeepTile;
-
-    // 海瓷砖
     public TileBase WaterTile;
-
-    // 浅滩瓷砖
     public TileBase BeachTile;
-    // 沙地瓷砖
     public TileBase SandTile;
-    // 泥土瓷砖
     public TileBase GroundTile;
-    // 高山
     public TileBase MountainPeakTile;
-    // 雪
     public TileBase SnowTile;
-    // 河
     public TileBase RiverTile;
 
-    [Header("tileVale")]
+    [Header("Elevation Levels")]
     [Range(0f, 1f)]
     public float deepLevel;
     [Range(0f, 1f)]
@@ -55,160 +41,143 @@ public class WorldTileMap : MonoBehaviour
     public float mountainLevel;
     [Range(0f, 1f)]
     public float snowLevel;
-    [Range(0.1f, 0f)]
+    [Range(0.1f, 1f)]
     public float offset;
 
-    [Header("River")]
-    public int RiverNumber;
-    public int RiverMaxLong;
-    [Header("Noise")]
+    [Header("Rivers")]
+    public int riverNumber;
+    public int riverMaxLength;
 
+    [Header("Noise Settings")]
     [Range(1, 8)]
-    public int octaves = 1; // 噪声分形程度滑条
+    public int octaves = 1;
     [Range(1f, 4f)]
-    public float frequencyMultiplier = 2f; // 频率倍增因子
+    public float frequencyMultiplier = 2f;
     [Range(0.1f, 1f)]
-    public float amplitudeReducer = 0.5f; // 振幅衰减因子
+    public float amplitudeReducer = 0.5f;
 
     private float[,] mapData;
-    WorldTileMapData[,] worldTileMapDataArray;
+    private WorldTileMapData[,] worldTileMapDataArray;
 
-
-    // 生成地图的方法
     public void MakeMap()
     {
         mapData = new float[maxMapSize.x, maxMapSize.y];
         worldTileMapDataArray = new WorldTileMapData[maxMapSize.x, maxMapSize.y];
         GenerateMap();
-        GenerateRiver();
-
+        GenerateRivers();
     }
 
-    // 生成地图的具体实现
     public void GenerateMap()
     {
         ClearMap();
-        // 如果没有指定种子，则使用当前时间作为种子
-        if (RandomSeed)
+        InitializeRandomSeed();
+
+        float randomOffset = UnityEngine.Random.Range(-1000, 1000);
+        for (int x = 0; x < maxMapSize.x; x++)
+        {
+            for (int y = 0; y < maxMapSize.y; y++)
+            {
+                float noiseValue = GenerateNoise(x, y, randomOffset);
+                mapData[x, y] = noiseValue;
+
+                SetTileAtPosition(x, y, noiseValue);
+            }
+        }
+    }
+
+    private void GenerateRivers()
+    {
+        UnityEngine.Random.InitState(seed / 2);
+        float randomOffset = UnityEngine.Random.Range(-1000, 1000);
+
+        for (int x = 0; x < maxMapSize.x; x++)
+        {
+            for (int y = 0; y < maxMapSize.y; y++)
+            {
+                float noiseValue = GenerateNoise(x, y, randomOffset, 0.5f);
+                if (noiseValue > 0.5f && noiseValue < 0.51f && mapData[x, y] > beachLevel)
+                {
+                    SetTileAtPosition(x, y, noiseValue, RiverTile, 7);
+                }
+            }
+        }
+    }
+
+    private float GenerateNoise(int x, int y, float randomOffset, float scale = 1f)
+    {
+        // 生成噪声值并进行归一化处理
+        // Generate noise value and normalize it
+        float a = 0f;
+        float amplitude = 1f;
+        float frequency = 1f;
+        for (int i = 0; i < octaves; i++)
+        {
+            a += Mathf.PerlinNoise((x * offset * scale + randomOffset) * frequency, (y * offset * scale + randomOffset) * frequency) * amplitude;
+            frequency *= frequencyMultiplier;
+            amplitude *= amplitudeReducer;
+        }
+        return a / (2f * (1f - Mathf.Pow(amplitudeReducer, octaves)));
+    }
+
+    private void SetTileAtPosition(int x, int y, float noiseValue, TileBase tile = null, int climateType = -1)
+    {
+        // 设置指定位置的瓷砖并记录地形数据
+        // Set the tile at the specified position and record the terrain data
+        if (tile == null)
+        {
+            tile = GetTileByNoiseValue(noiseValue);
+            climateType = GetClimateTypeByNoiseValue(noiseValue);
+        }
+
+        tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+
+        int elevation = (int)((noiseValue - beachLevel) * 100);
+        string name = "12"; // 这里可以根据实际需求进行修改
+        WorldTileMapData worldTileMapData = new WorldTileMapData(name, new Vector2Int(x, y), elevation, climateType);
+        worldTileMapDataArray[x, y] = worldTileMapData;
+    }
+
+    private TileBase GetTileByNoiseValue(float noiseValue)
+    {
+        // 根据噪声值获取对应的瓷砖
+        // Get the corresponding tile based on the noise value
+        if (noiseValue <= deepLevel) return DeepTile;
+        if (noiseValue <= waterLevel) return WaterTile;
+        if (noiseValue <= beachLevel) return BeachTile;
+        if (noiseValue <= sandLevel) return SandTile;
+        if (noiseValue <= groundLevel) return GroundTile;
+        if (noiseValue <= mountainLevel) return MountainPeakTile;
+        return SnowTile;
+    }
+
+    private int GetClimateTypeByNoiseValue(float noiseValue)
+    {
+        // 根据噪声值获取对应的气候类型
+        // Get the corresponding climate type based on the noise value
+        if (noiseValue <= deepLevel) return 0; // 深海 Deep Sea
+        if (noiseValue <= waterLevel && noiseValue > deepLevel) return 1; // 海水 Water
+        if (noiseValue <= beachLevel && noiseValue > waterLevel) return 2; // 浅滩 Beach
+        if (noiseValue <= sandLevel && noiseValue > beachLevel) return 3; // 沙地 Sand
+        if (noiseValue <= groundLevel && noiseValue > sandLevel) return 4; // 泥土 Ground
+        if (noiseValue <= mountainLevel) return 5; // 高山 Mountain Peak
+        return 6; // 雪 Snow
+    }
+
+    private void ClearMap()
+    {
+        // 清除地图上的所有瓷砖
+        // Clear all tiles on the map
+        tilemap.ClearAllTiles();
+    }
+
+    private void InitializeRandomSeed()
+    {
+        // 初始化随机种子
+        // Initialize the random seed
+        if (useCustomSeed)
         {
             seed = Time.time.GetHashCode();
         }
         UnityEngine.Random.InitState(seed);
-        float randomoffset = UnityEngine.Random.Range(-1000, 1000);
-        for (int X = 0; X < maxMapSize.x; X++)
-        {
-            for (int Y = 0; Y < maxMapSize.y; Y++)
-            {
-                float a = 0f;
-                float amplitude = 1f;
-                float frequency = 1f;
-                for (int i = 0; i < octaves; i++)
-                {
-                    a += Mathf.PerlinNoise((X * offset + randomoffset) * frequency, (Y * offset + randomoffset) * frequency) * amplitude;
-                    frequency *= frequencyMultiplier;
-                    amplitude *= amplitudeReducer;
-                }
-                a /= 2f * (1f - Mathf.Pow(amplitudeReducer, octaves)); // 归一化
-                mapData[X, Y] = a;
-
-                int elevation;
-                int climateType = -1;
-                if (a <= beachLevel)
-                {
-                    elevation = 0;
-                }
-                else
-                {
-                    elevation = (int)((a - beachLevel) * 100);
-                }
-                string name = "12";
-
-
-
-                if (a <= deepLevel)
-                {
-                    tilemap.SetTile(new Vector3Int(X, Y, 0), DeepTile);
-                    climateType = 0;
-                }
-                else if (a > deepLevel && a <= waterLevel)
-                {
-
-                    tilemap.SetTile(new Vector3Int(X, Y, 0), WaterTile);
-                    climateType = 1;
-                }
-                else if (a > waterLevel && a <= beachLevel)
-                {
-                    tilemap.SetTile(new Vector3Int(X, Y, 0), BeachTile);
-                    climateType = 2;
-                }
-                else if (a > beachLevel && a <= sandLevel)
-                {
-                    tilemap.SetTile(new Vector3Int(X, Y, 0), SandTile);
-                    climateType = 3;
-                }
-                else if (a > sandLevel && a <= groundLevel)
-                {
-                    tilemap.SetTile(new Vector3Int(X, Y, 0), GroundTile);
-                    climateType = 4;
-                }
-                else if (a > groundLevel && a <= mountainLevel)
-                {
-                    tilemap.SetTile(new Vector3Int(X, Y, 0), MountainPeakTile);
-                    climateType = 5;
-                }
-                else if (a > mountainLevel)
-                {
-                    tilemap.SetTile(new Vector3Int(X, Y, 0), SnowTile);
-                    climateType = 6;
-                }
-
-
-                WorldTileMapData worldTileMapData = new WorldTileMapData(name, new Vector2Int(X, Y), elevation, climateType);
-                worldTileMapDataArray[X, Y] = worldTileMapData;
-
-
-            }
-        }
-    }
-
-
-    // 生成河流
-    private void GenerateRiver()
-    {
-        UnityEngine.Random.InitState(seed / 2);
-        float randomoffset = UnityEngine.Random.Range(-1000, 1000);
-        for (int X = 0; X < maxMapSize.x; X++)
-        {
-            for (int Y = 0; Y < maxMapSize.y; Y++)
-            {
-                float a = 0f;
-                float amplitude = 1f;
-                float frequency = 1f;
-                for (int i = 0; i < octaves; i++)
-                {
-                    a += Mathf.PerlinNoise((X * offset / 2 + randomoffset) * frequency, (Y * offset / 2 + randomoffset) * frequency) * amplitude;
-                    frequency *= frequencyMultiplier;
-                    amplitude *= amplitudeReducer;
-                }
-                a /= 2f * (1f - Mathf.Pow(amplitudeReducer, octaves)); // 归一化
-                if (a > 0.5f && a < 0.51f && mapData[X, Y] > beachLevel)
-                {
-                    tilemap.SetTile(new Vector3Int(X, Y, 0), BeachTile);
-                    int elevation = (int)((a - beachLevel) * 100);
-                    WorldTileMapData worldTileMapData = new WorldTileMapData(name, new Vector2Int(X, Y), elevation, 7);
-                    worldTileMapDataArray[X, Y] = worldTileMapData;
-                }
-            }
-        }
-    }
-
-
-
-    // 清除地图上的所有瓷砖
-    public void ClearMap()
-    {
-        tilemap.ClearAllTiles();
-
-
     }
 }
